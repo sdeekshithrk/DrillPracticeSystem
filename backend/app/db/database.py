b01933f9baf3455f7244e2c98111b57f34a8d5b0
sdeekshithrk <sdeekshithrk@gmail.com>
2025-12-02 03:45:26 -0500

    DB changes

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -9,7 +9,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL not set in .env file")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+                       pool_pre_ping=True,
+                       connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
